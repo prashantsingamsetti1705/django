186e48b03e4d3f5faf75c4c1f38842c967ad0592
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-21 10:10:56 +0530

    form

--- from1/testapp/form.py
+++ from1/testapp/form.py
@@ -2,3 +2,5 @@ from django import forms
 class StudentFrom(forms.Form):
     name=forms.CharField()
     marks=forms.IntegerField()
+    email=forms.EmailField()
+    feed_back=forms.CharField(widget=forms.Textarea)
