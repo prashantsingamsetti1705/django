9b6df193f16d2a337412749efc0a2dec630437c3
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-23 09:56:13 +0530

    form

--- from1/testapp/form.py
+++ from1/testapp/form.py
@@ -1,6 +1,18 @@
 from django import forms
-class StudentFrom(forms.Form):
+from django.core import validators
+# def starts_with_s(value):
+#     print('starts_with_s function execution')
+#     if value[0].lower() != 's':
+#         raise forms.ValidationError('Name should be starts with s or S')
+class StudentForm(forms.Form):
     name=forms.CharField()
     marks=forms.IntegerField()
     email=forms.EmailField()
-    feed_back=forms.CharField(widget=forms.Textarea)
+    feed_back=forms.CharField(widget=forms.Textarea,validators=[
+	validators.MaxLengthValidator(40),validators.MinLengthValidator(10)])
+    def clean_name(self):
+        print("validting the name...")
+        input_name=self.cleaned_data['name']
+        if len(input_name) < 4:
+            raise forms.ValidationError("mainimum number of the char it four")
+        return input_name
--- from1/testapp/views.py
+++ from1/testapp/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from testapp.form import StudentFrom
+from testapp.form import StudentForm
 def student_input_view(request):
     submitted=False
     sname=''
     if request.method=='POST':
-        form=StudentFrom(request.POST)
+        form=StudentForm(request.POST)
         if form.is_valid():
             print("form valudtaion sucees full")
             print("marks:",form.cleaned_data['marks'])
             print("name",form.cleaned_data['name'])
             print("email",form.cleaned_data["email"])
             print("feddback",form.cleaned_data["feed_back"])
-        sname=form.cleaned_data['name']
-        submitted=True
-    form=StudentFrom()
-    my_dict={'form':form,'submitted':submitted,'sname':sname}
+            sname=form.cleaned_data['name']
+            submitted=True
+    else:
+        form=StudentForm()
+    my_dict={'form':form,'sname':sname}
     return render(
         request,'testapp/index.html',context=my_dict,)
 # Create your views here.
