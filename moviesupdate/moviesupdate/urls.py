9a773c5c7e04d810fd9fee5579a476e77593a773
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-29 10:14:55 +0530

    movie

--- moviesupdate/moviesupdate/urls.py
+++ moviesupdate/moviesupdate/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from testapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.movie_update),
+    path('mlist',views.movielist),
+    path('addmovie/',views.add_movie)
 ]
--- moviesupdate/testapp/views.py
+++ moviesupdate/testapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+def movie_update(request):
+    return render(request,'testapp/index.html')
+from testapp.models import Student
+def movielist(request):
+    movielist=Student.objects.all()
+    return render(request,'testapp/movielist.html',{'movielist':movielist})
 # Create your views here.
+from testapp.forms import StudentForm
+def add_movie(request):
+    if request.method=="POST":
+        form=StudentForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            print('Record inserted into DB successfully.....')
+    form = StudentForm()
+    return render(request,'testapp/addmovie.html',{'form':form})
+
+
